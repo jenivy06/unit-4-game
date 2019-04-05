86eac3bf9eb1cb8c6524ccedb71faf8659fd71e9
Jenny Ivy <47339542+jenivy06@users.noreply.github.com>
2019-04-05 10:35:44 -0600

    added javascript for first round of game

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,7 +1,10 @@
 //  Crystals Collector Game
 var won = 0;
+document.getElementById("wins").innerHTML = "Wins: " + won;
 var lost = 0;
+document.getElementById("losses").innerHTML = "Losses: " + lost;
 var yourTotalScore = 0;
+document.getElementById("yourTotalScore").innerHTML = "Your total score: " + yourTotalScore;
 
 // Have the computer randomly pick a number between 19 and 120 that will become the target score.
     var min=19; 
@@ -9,9 +12,10 @@ var yourTotalScore = 0;
     var targetScore =Math.floor(Math.random() * (+max - +min)) + +min; 
     console.log(targetScore);
 
+
     // Locate on HTML where the target score will be posted
 
-    $('score-to-match-text').text(targetScore);
+    document.getElementById("score-to-match-text").innerHTML = "Number to match: " + targetScore;
 
 
 //Show images of four crystals. Each crystal will have a separate numerical value (each will have a value randomly generated between 1 and 12).
@@ -30,6 +34,7 @@ var yourTotalScore = 0;
 //The user can click on a crystal image to see the value of that image. Each time a crystal image is clicked, it gets added to the user's total score.
     //Locate on HTML where crystal images will show    
     
+    var counter = 0;
     var crystalnumberArray1 = [11, 8, 4, 2];
     for (var i = 0; i < crystalnumberArray1.length; i++) {
         console.log(crystalnumberArray1);
@@ -37,19 +42,57 @@ var yourTotalScore = 0;
         imageCrystal.addClass("crystal-image");
         imageCrystal.attr("src", "https://cdn.pixabay.com/photo/2016/08/18/17/37/rock-crystal-1603480_960_720.jpg");
         imageCrystal.attr("data-crystalvalue", crystalnumberArray1[i]);
+        console.log(crystalnumberArray1[i]);
         $("#crystals").append(imageCrystal);
-  }
+    }
+  
+    $(".crystal-image").on("click", function() {
+        var crystalValue = ($(this).attr("data-crystalvalue"));
+        console.log(this);
+        crystalValue = parseInt(crystalValue);
+        counter += crystalValue;
+        $("#yourTotalScore").text("Your total score: " + counter);
+        console.log("counter", counter);
+        console.log("crystal value", crystalValue);
+        //alert("New score: " + counter);
 
     //Locate on HTML where user's score is posted
 //If the user reaches the target score without going over:
     //Show an alert, "You win!"
     //Increase number of wins by one
-        //Locate on HTML where number of wins is posted.
-    //Generate a new random target score
-    //Reset the values for each crystal image.
+    if (counter === targetScore) {
+        alert("You win!");
+        won++;
+        $("wins").text("Wins " + won);
+        //Reset counter and generate new values for each crystal
+        var counter = 0;
+        var crystalnumberArray2 = [10, 1, 4, 12];
+        // begin another game loop with new crystal values
+        for (var i = 0; i < crystalnumberArray2.length; i++) {
+            var imageCrystal = $("<img>");
+            imageCrystal.addClass("crystal-image");
+            imageCrystal.attr("src", "https://cdn.pixabay.com/photo/2016/08/18/17/37/rock-crystal-1603480_960_720.jpg");
+            imageCrystal.attr("data-crystalvalue", crystalnumberArray2[i]);
+            $("#crystals").append(imageCrystal);
+      }
+
 //If the user's score goes over the target score:
     //Show an alert, "You lose!"
     //Increase number of losses by one
         //locate on HTML where number of losses is posted
-        //Generate a new random target score
-        //Reset the values for each crystal image.
+    } else if (counter > targetScore) {
+        alert("You lose!!");
+        lost++;
+        $("losses").text("Losses: " + lost);
+        //Reset counter and generate new values for each crystal
+        var counter = 0;
+        var crystalnumberArray2 = [10, 1, 4, 12];
+        // begin another game loop with new crystal values
+        for (var i = 0; i < crystalnumberArray2.length; i++) {
+            var imageCrystal = $("<img>");
+            imageCrystal.addClass("crystal-image");
+            imageCrystal.attr("src", "https://cdn.pixabay.com/photo/2016/08/18/17/37/rock-crystal-1603480_960_720.jpg");
+            imageCrystal.attr("data-crystalvalue", crystalnumberArray2[i]);
+            $("#crystals").append(imageCrystal);
+      }
+    }
