3bd4ac899a5a7068870758a881a639d6777aaabb
Jenny Ivy <47339542+jenivy06@users.noreply.github.com>
2019-04-14 20:36:56 -0600

    updated jquery and javascript

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,44 +1,45 @@
+$(document).ready(function() {
 var won = 0;
-document.getElementById("wins").innerHTML = "Wins: " + won;
+$("#wins").text("Wins: " + won);
+//document.getElementById("wins").innerHTML = "Wins: " + won;
 var lost = 0;
-document.getElementById("losses").innerHTML = "Losses: " + lost;
+//document.getElementById("losses").innerHTML = "Losses: " + lost;
+$("#losses").text("Lost: " + lost);
 var yourTotalScore = 0;
-document.getElementById("yourTotalScore").innerHTML = "Your total score: " + yourTotalScore;
+$("#yourTotalScore").text("Your total score: " + yourTotalScore);
+//document.getElementById("yourTotalScore").innerHTML = "Your total score: " + yourTotalScore;
 // Start the game. Have the computer randomly pick a number between 19 and 120 that will become the target score.
-//function startGame ()
-    var min=19; 
+var min=19; 
     var max=120;  
     var targetScore =Math.floor(Math.random() * (+max - +min)) + +min; 
     console.log(targetScore);
     // Locate on HTML where the target score will be posted
     document.getElementById("score-to-match-text").innerHTML = "Number to match: " + targetScore;
-//Show images of four crystals. Each crystal will have a separate numerical value (each will have a value randomly generated between 1 and 12).
-    //var min=1; 
-    ///var max=12;  
-    //var crystalNumber1 = Math.floor(Math.random() * (+max - +min)) + +min; 
-    ///console.log(crystalNumber1);
-    //var crystalNumber2 = Math.floor(Math.random() * (+max - +min)) + +min; 
-    //console.log(crystalNumber2);
-    //var crystalNumber3 = Math.floor(Math.random() * (+max - +min)) + +min; 
-    //console.log(crystalNumber3);
-    //var crystalNumber4 = Math.floor(Math.random() * (+max - +min)) + +min; 
-    //console.log(crystalNumber4);
-    
+    //Show images of four crystals. Each crystal will have a separate numerical value (each will have a value randomly generated between 1 and 12).
+    var min=1; 
+    var max=12;  
+    var crystalNumber1 = Math.floor(Math.random() * (+max - +min)) + +min; 
+    console.log(crystalNumber1);
+    var crystalNumber2 = Math.floor(Math.random() * (+max - +min)) + +min; 
+    console.log(crystalNumber2);
+    var crystalNumber3 = Math.floor(Math.random() * (+max - +min)) + +min; 
+    console.log(crystalNumber3);
+    var crystalNumber4 = Math.floor(Math.random() * (+max - +min)) + +min; 
+    console.log(crystalNumber4);
 //The user can click on a crystal image to see the value of that image. Each time a crystal image is clicked, it gets added to the user's total score.
     //Locate on HTML where crystal images will show    
     
     var counter = 0;
-    var crystalnumberArray1 = [11, 8, 4, 2];
+    var crystalnumberArray1 = [crystalNumber1, crystalNumber2, crystalNumber3, crystalNumber4];
     for (var i = 0; i < crystalnumberArray1.length; i++) {
         console.log(crystalnumberArray1);
         var imageCrystal = $("<img>");
         imageCrystal.addClass("crystal-image");
-        imageCrystal.attr("src", "https://cdn.pixabay.com/photo/2016/08/18/17/37/rock-crystal-1603480_960_720.jpg");
+        imageCrystal.attr("src", "assets/images/whitecrystal.jpg");
         imageCrystal.attr("data-crystalvalue", crystalnumberArray1[i]);
         console.log(crystalnumberArray1[i]);
         $("#crystals").append(imageCrystal);
     }
-  
     $(".crystal-image").on("click", function() {
         var crystalValue = ($(this).attr("data-crystalvalue"));
         console.log(this);
@@ -47,35 +48,123 @@ document.getElementById("yourTotalScore").innerHTML = "Your total score: " + you
         $("#yourTotalScore").text("Your total score: " + counter);
         console.log("counter", counter);
         console.log("crystal value", crystalValue);
-        //alert("New score: " + counter);
-    //Locate on HTML where user's score is posted
-//If the user reaches the target score without going over:
-    //Show an alert, "You win!"
-    //Increase number of wins by one
-    if (counter === targetScore) {
-        alert("You win!!");
-        won++;
-        document.getElementById("wins").innerHTML = "Wins: " + won;
-        //Reset counter and generate new values for each crystal
-      }
-//If the user's score goes over the target score:
-    //Show an alert, "You lose!"
-    //Increase number of losses by one
-        //locate on HTML where number of losses is posted
-    else if (counter > targetScore) {
-        alert("You lose!!");
-        lost++;
-        document.getElementById("losses").innerHTML = "Losses: " + lost;
-        //Clear user's total score
-        //var yourNewTotalScore = 0;
-        //document.getElementById("yourTotalScore").innerHTML = "Your total score: " + yourNewTotalScore;
-        //Clear crystals div
-        //document.getElementById("crystals").innerHTML = "";
-        //Create new score to match
-        //var min=19; 
-        //var max=120;  
-       // var newtargetScore =Math.floor(Math.random() * (+max - +min)) + +min; 
-        //console.log(newtargetScore);
-        // Locate on HTML where the target score will be posted
-       // document.getElementById("score-to-match-text").innerHTML = "Number to match: " + newtargetScore; 
-    }
+        if (counter === targetScore) {
+            alert("You win!! Play again?");
+            won++;
+            $("#wins").text("Wins: " + won);
+            //Clear user's total score
+            var yourNewTotalScore = 0;
+            $("#yourTotalScore").text("Your total score: " + yourNewTotalScore);
+            //Clear crystals div
+            document.getElementById("crystals").innerHTML = "";
+            //Create new score to match
+            var min=19; 
+            var max=120;  
+            var newtargetScore =Math.floor(Math.random() * (+max - +min)) + +min; 
+            console.log(newtargetScore);
+            // Locate on HTML where the target score will be posted
+            document.getElementById("score-to-match-text").innerHTML = "Number to match: " + newtargetScore;
+            //create new crystal images with new attributes and data values
+            var min=1; 
+            var max=12;  
+            var crystalNumber1 = Math.floor(Math.random() * (+max - +min)) + +min; 
+            console.log(crystalNumber1);
+            var crystalNumber2 = Math.floor(Math.random() * (+max - +min)) + +min; 
+            console.log(crystalNumber2);
+            var crystalNumber3 = Math.floor(Math.random() * (+max - +min)) + +min; 
+            console.log(crystalNumber3);
+            var crystalNumber4 = Math.floor(Math.random() * (+max - +min)) + +min; 
+            console.log(crystalNumber4);
+            var newCounter = 0;
+            var crystalnumberArray2 = [crystalNumber1, crystalNumber2, crystalNumber3, crystalNumber4];
+            console.log(crystalnumberArray2);
+            for (var i = 0; i < crystalnumberArray2.length; i++) {
+                var imageCrystal = $("<img>");
+                imageCrystal.addClass("crystal-image");
+                imageCrystal.attr("src", "assets/images/purplecrystal.jpg");
+                imageCrystal.attr("data-crystalvalue", crystalnumberArray2[i]);
+                $("#crystals").append(imageCrystal);
+            }
+            // Create new function that lets users click on image and image will have a numerical value that posts to "Your Total Score".
+            $(".crystal-image").on("click", function() {
+                var crystalValue = ($(this).attr("data-crystalvalue"));
+                console.log(this);
+                crystalValue = parseInt(crystalValue);
+                newCounter += crystalValue;
+                $("#yourTotalScore").text("Your total score: " + newCounter);
+                console.log("counter", newCounter);
+                console.log("crystal value", crystalValue);
+                if (newCounter > newtargetScore) {
+                    alert("You lost again! Want to play more? Refresh this page.");
+                    lost++;
+                    document.getElementById("losses").innerHTML = "Losses: " + lost;
+                    //Clear user's total score
+                    var yourNewTotalScore = 0;
+                    document.getElementById("yourTotalScore").innerHTML = "Your total score: " + yourNewTotalScore;
+                }
+                else if (newCounter === newtargetScore) {
+                    alert("Congrats, you won! Refresh the page to keep playing!");
+                }
+            });
+        }
+        else if (counter > targetScore) {
+            alert("You lose!! Play again?");
+            lost++;
+            document.getElementById("losses").innerHTML = "Losses: " + lost;
+            //Clear user's total score
+            var yourNewTotalScore = 0;
+            document.getElementById("yourTotalScore").innerHTML = "Your total score: " + yourNewTotalScore;
+            //Clear crystals div
+            document.getElementById("crystals").innerHTML = "";
+            //Create new score to match
+            var min=19; 
+            var max=120;  
+            var newCounter = 0;
+            var newtargetScore =Math.floor(Math.random() * (+max - +min)) + +min; 
+            console.log(newtargetScore);
+            // Locate on HTML where the target score will be posted
+            document.getElementById("score-to-match-text").innerHTML = "Number to match: " + newtargetScore;
+            //create new crystal images with new attributes and data values
+            var min=1; 
+            var max=12;  
+            var crystalNumber1 = Math.floor(Math.random() * (+max - +min)) + +min; 
+            console.log(crystalNumber1);
+            var crystalNumber2 = Math.floor(Math.random() * (+max - +min)) + +min; 
+            console.log(crystalNumber2);
+            var crystalNumber3 = Math.floor(Math.random() * (+max - +min)) + +min; 
+            console.log(crystalNumber3);
+            var crystalNumber4 = Math.floor(Math.random() * (+max - +min)) + +min; 
+            console.log(crystalNumber4);
+            var crystalnumberArray2 = [crystalNumber1, crystalNumber2, crystalNumber3, crystalNumber4];
+            console.log(crystalnumberArray2);
+            for (var i = 0; i < crystalnumberArray2.length; i++) {
+                var imageCrystal = $("<img>");
+                imageCrystal.addClass("crystal-image");
+                imageCrystal.attr("src", "assets/images/purplecrystal.jpg");
+                imageCrystal.attr("data-crystalvalue", crystalnumberArray2[i]);
+                $("#crystals").append(imageCrystal);
+            }
+            // Create new function that lets users click on image and image will have a numerical value that posts to "Your Total Score".
+        $(".crystal-image").on("click", function() {
+            var crystalValue = ($(this).attr("data-crystalvalue"));
+            console.log(this);
+            crystalValue = parseInt(crystalValue);
+            newCounter += crystalValue;
+            $("#yourTotalScore").text("Your total score: " + newCounter);
+            if (newCounter > newtargetScore) {
+                alert("You lost again! Want to play more? Refresh this page.");
+                lost++;
+                document.getElementById("losses").innerHTML = "Losses: " + lost;
+                //Clear user's total score
+                var yourNewTotalScore = 0;
+                document.getElementById("yourTotalScore").innerHTML = "Your total score: " + yourNewTotalScore;
+            }
+            else if (newCounter === newtargetScore) {
+                alert("Congrats, you won! Refresh the page to keep playing!");
+            }
+        });       
+        }
+        
+        
+    });
+});
